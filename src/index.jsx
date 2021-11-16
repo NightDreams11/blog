ff8cd2c96c92ddfd4fa0cbd441a3aae15a9ec2ce
rettimo <therettimo@gmail.com>
2021-11-16 16:21:35 +0200

    update

--- src/index.jsx
+++ src/index.jsx
@@ -4,16 +4,16 @@ import { GlobalStyles } from 'styles/global'
 import { Normalize } from 'styles/normalize'
 import { theme } from 'styles/theme'
 import { ThemeProvider } from 'styled-components'
-import { Provider } from 'react-redux'
+import { Provider as StoreProvider } from 'react-redux'
 import { store } from 'store'
 
 render(
-  <Provider store={store}>
+  <StoreProvider store={store}>
     <ThemeProvider theme={theme}>
       <Normalize />
       <GlobalStyles />
       <App />
     </ThemeProvider>
-  </Provider>,
+  </StoreProvider>,
   document.getElementById('root')
 )
